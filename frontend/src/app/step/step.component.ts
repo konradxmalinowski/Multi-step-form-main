7732528647e4f612f6074f6ecb1e472efb49d5c0
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-09-30 21:21:11 +0200

    Add pfirst part of step component

--- frontend/src/app/step/step.component.ts
+++ frontend/src/app/step/step.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { STEPS_LIST } from './constants';
+import { Form } from '../types/form';
 
 type state = 1 | 2 | 3 | 4;
 
@@ -8,9 +10,44 @@ type state = 1 | 2 | 3 | 4;
   styleUrls: ['./step.component.scss']
 })
 export class StepComponent {
-  private formState: state = 1;
+  formState: state = 1;
+  steps = STEPS_LIST;
+
+  data: Form = {
+    name: '',
+    email: '',
+    phoneNumber: '',
+    plan: '',
+    planType: '',
+    onlineService: false,
+    largerStorage: false,
+    customizableProfile: false,
+  };
+
+  handleChangeData<K extends keyof Form>(property: K, newValue: Form[K]) {
+    this.data[property] = newValue;
+  }
+
+  validateInsertedData(): boolean {
+    switch (this.formState) {
+      case 1: {
+        if (!this.data.name.trim()) return false;
+        if (!this.data.email.trim()) return false;
+        if (!this.data.phoneNumber.trim()) return false;
+        break;
+      };
+      case 2: {
+        if (!this.data.plan.trim()) return false;
+        if (!this.data.planType.trim()) return false;
+        break;
+      };
+      default: return true;
+    }
+    return true;
+  }
 
   nextStep() {
+    if (this.validateInsertedData()) { }
     if (this.formState < 4) this.formState++;
   }
 
