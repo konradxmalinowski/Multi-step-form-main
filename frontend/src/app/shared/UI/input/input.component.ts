820b020e94c9a64561f1364b0374917bb2a44aeb
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-09-30 21:18:59 +0200

    Add first part of input component

--- frontend/src/app/shared/UI/input/input.component.ts
+++ frontend/src/app/shared/UI/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./input.component.scss']
 })
 export class InputComponent {
+  @Input() label: string = '';
+  @Input() type: string = 'text';
+  @Output() handleChange = new EventEmitter<string>();
 
+  handleChangeValue(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.handleChange.emit(input.value);
+  }
 }
