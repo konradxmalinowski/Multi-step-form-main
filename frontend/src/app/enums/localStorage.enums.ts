c2aa8f54453998c03600b91870c11a609667354c
konradxmalinowski <malinowski.konrad45@gmail.com>
2025-10-04 22:43:29 +0200

    Add enums for properties names

---
+++ frontend/src/app/enums/localStorage.enums.ts
@@ -0,0 +1,11 @@
+export enum PropertiesNames {
+    id = 'id',
+    name = 'name',
+    email = 'email',
+    phoneNumber = 'phoneNumber',
+    plan = 'plan',
+    planType = 'planType',
+    onlineService = 'onlineService',
+    largerStorage = 'largerStorage',
+    customizableProfile = 'customizableProfile'
+}
